f39b636f6bb000d537ec735ba37214fa520d0873
jeanbuhendwa <jeanjacquesirenge40@gmail.com>
2023-11-04 00:52:45 +0300

    update tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,21 @@
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: "hsl(234, 29%, 20%)",
+        bgColor: "hsl(0, 0%, 100%)",
+        bgOrange: "hsl(4, 100%, 67%)",
+        textColor: "hsl(231, 7%, 60%)",
+        textColorDark: "hsl(234, 29%, 20%)",
+      },
+    },
+    screens: {
+      sm: "640px",
+      md: "768px",
+      lg: "1024px",
+      xl: "1280px",
+    },
   },
   plugins: [],
 };
